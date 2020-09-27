fa427e9a01a4632fed11befdd3342f638fc10b74
Andrew Wooldridge <triptych@gmail.com>
2020-09-26 23:58:07 -0700

    server.js update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 app.get("/login/github", (req, res) => {
   const redirect_uri = "http://localhost:9000/login/github/callback";
   res.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${redirect_uri}`
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${redirect_uri}&scope=gist`
   );
 });
 
@@ -50,6 +50,34 @@ async function fetchGitHubUser(token) {
   return await request.json();
 }
 
+async function setGist(token) {
+  // post and get response here
+  const nDate = new Date();
+
+  const request = await fetch("https://api.github.com/gists", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/vnd.github.v3+json",
+      Authorization: "token " + token
+    },
+    body:JSON.stringify({
+      "description": "a test gist!",
+      "files": {
+        "readme.txt": {
+          "content": "Some stuff! please work! " + nDate.toDateString();
+        }
+      }
+    })
+  });
+
+  const text = await request.text();
+  //const myjson = await request.json();
+  console.log("text ::: ",text);
+  //console.log("json :::", myjson);
+  return text;
+}
+
 app.get("/login/github/callback", async (req, res) => {
   const code = req.query.code;
   const access_token = await getAccessToken({ code, client_id, client_secret });
@@ -66,7 +94,10 @@ app.get("/login/github/callback", async (req, res) => {
 app.get("/admin", async (req, res) => {
   console.log(req.session.githubId);
   if (req.session && req.session.githubId === 8691) {
-    res.send("Hello Andrew <pre>" + JSON.stringify(req.session, null, 2));
+    res.send(
+      "Hello Andrew <a href='/gist'>gist</a> <pre>" +
+        JSON.stringify(req.session, null, 2)
+    );
     // Possible use "fetchGitHubUser" with the access_token
   } else {
     // res.redirect("/login/github");
@@ -78,6 +109,16 @@ app.get("/logout", (req, res) => {
   if (req.session) req.session = null;
   res.redirect("/");
 });
+
+app.get("/gist", (req, res) => {
+  res.send("let's make a gist! <a href='/makegist'>makegist</a>");
+});
+
+app.get("/makegist", async (req,res)=>{
+  const mygist = await setGist(req.session.access_token);
+  console.log("Mygist:", mygist );
+  res.send("response: <pre>"+mygist);
+})
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log("Listening on localhost:" + PORT));
